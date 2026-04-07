76035ce997b36fa0f5cdb3ec915c56c94cf2807b
zqbq099 <zqbq099@gmail.com>
2026-04-07 21:28:44 +0300

    Build: تطبيق التمور الذكي v1.0.0

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-الكود في الملف المظغوط
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
